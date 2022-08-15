d0109d5555317a1f562bc0ac901e8489c4035c0c
jay-aree <jayyqwe123@gmail.com>
2022-08-15 08:08:19 +0700

    Node js getting started. Build routes and controllers

---
+++ 03-task-manager/starter/controllers/tasks.js
@@ -0,0 +1,21 @@
+const getAllTasks = (req, res) => {
+    res.send('all items from the file')
+}
+const createTask = (req, res) => {
+    res.json(req.body)
+}
+
+const getTask = (req, res) => {
+    res.json({id:req.params.id})
+}
+
+const updateTask = (req, res) => {
+    res.send('up')
+}
+
+const deleteTask = (req, res) => {
+    res.send({id:req.params.id})
+}
+
+
+module.exports = {getAllTasks,updateTask,deleteTask,createTask,getTask}
